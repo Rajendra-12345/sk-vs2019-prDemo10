da5039fb311ec5b1898c8ab6e5d8323191ef2fd9
Rajendra <mrajendra6819@gmail.com>
2021-11-16 12:19:02 +0530

    Rajendra added

--- sk-vs2019-prDemo10/Startup.cs
+++ sk-vs2019-prDemo10/Startup.cs
@@ -33,6 +33,7 @@ namespace sk_vs2019_prDemo10
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! rajendra");
                 });
             });
         }
